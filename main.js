297dc3ac7b2078cae71057121032700889a5848f
mziyadam <mziyadam@users.noreply.github.com>
2023-10-03 20:43:24 +0700

    udh jd tinggal rapihin

--- main.js
+++ main.js
@@ -4,35 +4,191 @@ const {
   ipcMain
 } = require('electron')
 const path = require('path')
-const srv = require('./server')
 
 var express = require('express');
+var exp = express();
+var sql = require('mssql');
 const cookieParser = require("cookie-parser");
+const expSession = require("express-session");
+const {
+  session
+} = require('electron')
 
-const session = require("express-session");
+exp.use(express.json());
+exp.use(express.urlencoded({
+  extended: true
+})); // to support JSON-encoded bodies
+
+exp.use(cookieParser());
+exp.use(expSession({
+  secret: "st2023st",
+  saveUninitialized: true,
+  resave: true
+}));
+
+exp.post('/logout', function (req, res) {
+  req.session.destroy()
+  sql.close()
+  res.send("Your are logged out ");
+})
+exp.post('/login', function (req, res) {
+  sql.close()
+  var param = req.body;
+  req.session.user = param.user
+  req.session.password = param.password
+  req.session.server = param.server
+  req.session.database = param.database
+  req.session.save()
+  var config = {
+    user: req.session.user,
+    password: req.session.password,
+    server: req.session.server,
+    database: req.session.database,
+    trustServerCertificate: true
+  };
+  sql.connect(config, function (err) {
+    var request = new sql.Request();
+    request.query("select 1 as ok", function (err, result) {
+      res.send(result)
+      req.session.loggedIn = 'y'
+      req.session.save()
+    })
+  });
+});
+
+exp.get('/', function (req, res) {
+  var config = {
+    user: req.session.user,
+    password: req.session.password,
+    server: req.session.server,
+    database: req.session.database,
+    trustServerCertificate: true
+  };
+
+  sql.connect(config, function (err) {
+    var request = new sql.Request();
+    request.query('select * from test', (err, result) => {
+      console.dir(result)
+      res.send(result);
+    })
+  });
+});
+
+exp.get('/surat', function (req, res) {
+  var query = '';
+  if (req.query.id) {
+    query += "@id = '" + req.query.id + "',";
+  }
+  if (req.query.no_agenda) {
+    query += "@no_agenda = '" + req.query.no_agenda + "',";
+  }
+  if (req.query.nomor_surat) {
+    query += "@nomor_surat = '" + req.query.nomor_surat + "',";
+  }
+  if (req.query.tanggal_surat) {
+    query += "@tanggal_surat = '" + req.query.tanggal_surat + "',";
+  }
+  if (req.query.tujuan) {
+    query += "@tujuan = '" + req.query.tujuan + "',";
+  }
+  if (req.query.perihal) {
+    query += "@perihal = '" + req.query.perihal + "',";
+  }
+  if (query != '') {
+    query = query.substring(0, query.length - 1);
+  }
+
+  var config = {
+    user: req.session.user,
+    password: req.session.password,
+    server: req.session.server,
+    database: req.session.database,
+    trustServerCertificate: true
+  };
+
+  sql.connect(config, function (err) {
+    var request = new sql.Request();
+    request.query(`exec GetSurat ${query}`, (err, result) => {
+      console.dir(result)
+      res.send(result);
+    })
+  });
+});
+
+exp.post('/test-post', function (req, res) {
+  var param = req.body;
+  res.send(param);
+});
+
+
+exp.get('/tim', function (req, res) {
+  var config = {
+    user: req.session.user,
+    password: req.session.password,
+    server: req.session.server,
+    database: req.session.database,
+    trustServerCertificate: true
+  };
+
+  sql.connect(config, function (err) {
+    var request = new sql.Request();
+    request.query("select * from tim", (err, result) => {
+      console.dir(result)
+      res.send(result);
+    })
+  });
+});
+exp.get('/klasifikasi', function (req, res) {
+  var config = {
+    user: req.session.user,
+    password: req.session.password,
+    server: req.session.server,
+    database: req.session.database,
+    trustServerCertificate: true
+  };
+
+  sql.connect(config, function (err) {
+    var request = new sql.Request();
+    request.query("select * from klasifikasi", (err, result) => {
+      console.dir(result)
+      res.send(result);
+    })
+  });
+});
+
+exp.get('/main', function (req, res) {
+  res.send(req.session.loggedIn)
+  // r.send('tes')
+});
+
+exp.post('/generate-surat', function (req, res) {
+  var param = req.body;
+  var config = {
+    user: req.session.user,
+    password: req.session.password,
+    server: req.session.server,
+    database: req.session.database,
+    trustServerCertificate: true
+  };
+
+  sql.connect(config, function (err) {
+    var request = new sql.Request();
+    request.query(`exec AddSurat @nomor_surat = '${param.nomor_surat}', @tujuan='${param.tujuan}', @perihal='${param.perihal}', @jumlah=${param.jumlah}`, (err, result) => {
+      console.dir(result)
+      res.send(result);
+    });
+  });
+});
+
+var server = exp.listen(5000, function () {
+  console.log('Server is running..');
+});
 
 function handleSetTitle(event, title) {
   const webContents = event.sender
   const win = BrowserWindow.fromWebContents(webContents)
   win.setTitle(title)
 }
-var exp = express();
-exp.use(cookieParser());
-
-exp.use(session({
-  secret: "st2023st",
-  saveUninitialized: true,
-  resave: true
-}));
-
-exp.get('/main', function (q, r, n) {
-  r.send(q.session.loggedIn)
-  // r.send('tes')
-});
-
-exp.listen(4000, function () {
-  console.log('Server is running..');
-});
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
@@ -48,8 +204,21 @@ function createWindow() {
     const win = BrowserWindow.fromWebContents(webContents)
     win.setTitle(title)
   })
-
   mainWindow.loadFile('src/index.html')
+  // const ses = session.fromPartition('persist:name')
+  const child = new BrowserWindow({
+    parent: mainWindow,
+    modal: true,
+    show: false
+  })
+  child.loadFile('src/koneksi/konek.html')
+  if (session.loggedIn == null || session.loggedIn == undefined) {
+    // window.open('src/koneksi/konek.html', 'modal')
+    child.once('ready-to-show', () => {
+      child.show()
+    })
+  }
+  console.log(session.loggedIn)
   // mainWindow.webContents.setWindowOpenHandler(({ url }) => {
   //   if (url === 'about:blank') {
   //     return {
--- server.js
+++ server.js
@@ -1,90 +1,34 @@
 var express = require('express');
-var app = express();
-// var sql = require('mssql/msnodesqlv8');
+var exp = express();
 var sql = require('mssql');
-
 const cookieParser = require("cookie-parser");
-
 const session = require("express-session");
-// const pool = new sql.ConnectionPool({
-//     database: 'st2023',
-//     server: 'LAPTOP-HTLEJA3V',
-//     driver: 'msnodesqlv8',
-//     options: {
-//         trustedConnection: true
-//     }
-// })
-// var bodyParser = require('body-parser')
-app.use(express.json());
-app.use(express.urlencoded({
+
+exp.use(express.json());
+exp.use(express.urlencoded({
     extended: true
 })); // to support JSON-encoded bodies
-// app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-//   extended: true
-// })); 
-app.use(cookieParser());
 
-app.use(session({
+exp.use(cookieParser());
+exp.use(session({
     secret: "st2023st",
     saveUninitialized: true,
     resave: true
 }));
 
-// var pool = new sql.ConnectionPool({
-//     user: 'stadmin',
-//     password: 'bismillah',
-//     server: 'LAPTOP-HTLEJA3V',
-//     database: 'st2023',
-//     trustServerCertificate: true
-// });
-// pool.connect();
-// pool.disconnect();
-app.post('/logout', function (req, res) {
+exp.post('/logout', function (req, res) {
     req.session.destroy()
     sql.close()
     res.send("Your are logged out ");
 })
-app.post('/login', function (req, res) {
-
+exp.post('/login', function (req, res) {
     sql.close()
     var param = req.body;
-    // var pool = new sql.ConnectionPool({
-    //     user: param.user,
-    //     password: param.password,
-    //     server: param.server,
-    //     database: param.database,
-    //     trustServerCertificate: true
-    // });
-
     req.session.user = param.user
     req.session.password = param.password
     req.session.server = param.server
     req.session.database = param.database
     req.session.save()
-    // var pool = new sql.ConnectionPool({
-    //     user: req.session.user,
-    //     password: req.session.password,
-    //     server: req.session.server,
-    //     database: req.session.database,
-    //     trustServerCertificate: true
-    // });
-
-
-    //     var pool = new sql.ConnectionPool({
-    //     user: 'stadmin',
-    //     password: 'bismillah',
-    //     server: 'LAPTOP-HTLEJA3V',
-    //     database: 'st2023',
-    //     trustServerCertificate: true
-    // });
-    // var param = req.body;
-    // var pool = new sql.ConnectionPool({
-    //     user: param.user,
-    //     password: param.password,
-    //     server: param.server,
-    //     database: param.database,
-    //     trustServerCertificate: true
-    // });
     var config = {
         user: req.session.user,
         password: req.session.password,
@@ -100,51 +44,9 @@ app.post('/login', function (req, res) {
             req.session.save()
         })
     });
-    // sql.close();
-    // res.send(pool);
-    // pool.connect(function(err) {
-    //     // if (err) throw err;
-    //     // console.log("Connected!");
-
-    // // res.send(err);
-    //     // con.query(sql, function (err, result) {
-    //     //   if (err) throw err;
-    //     //   console.log("Result: " + result);
-    //     // });
-    //   }).then(pol=>{
-    //     res.send(pool);
-
-    //   });
-
-    // pool.query('SELECT 1 + 1 AS solution', (error, results) => {
-    //     // if (error) res.send(error);
-    //     // console.log('The solution is: ', results[0].solution);
-    //     res.send(results);
-    // });
-
-    // res.send(pool);
-    // res.send({
-    //     user: param.user,
-    //     password: param.password,
-    //     server: param.server,
-    //     database: param.database});
-    // pool.request().query('select * from test', (err, result) => {
-    //     console.dir(result)
-    //     res.send(result);
-    // })
 });
 
-app.get('/', function (req, res) {
-
-
-    //   const pool = new sql.ConnectionPool({
-    //         user: 'sa',
-    //     password: 'P@ssw0rd1',
-    //     server: '10.1.7.82', 
-    //     database: 'simpeg41' ,
-    //     trustServerCertificate: true
-    // });
-
+exp.get('/', function (req, res) {
     var config = {
         user: req.session.user,
         password: req.session.password,
@@ -160,19 +62,9 @@ app.get('/', function (req, res) {
             res.send(result);
         })
     });
-    //simple query
-    // pool.request().query('select * from test', (err, result) => {
-    //     console.dir(result)
-    //     res.send(result);
-    // })
-    //query from server
-    // pool.request().query('select * from temp_log_test', (err, result) => {
-    //   console.dir(result)
-    //   res.send(result);
-    // })
 });
 
-app.get('/surat', function (req, res) {
+exp.get('/surat', function (req, res) {
     var query = '';
     if (req.query.id) {
         query += "@id = '" + req.query.id + "',";
@@ -211,31 +103,15 @@ app.get('/surat', function (req, res) {
             res.send(result);
         })
     });
-    // res.send("exec GetSurat " + query);
-    //simple query
-    // pool.request().query(`exec GetSurat ${query}`, (err, result) => {
-    //     console.dir(result)
-    //     res.send(result);
-    // })
 });
 
-app.post('/test-post', function (req, res) {
+exp.post('/test-post', function (req, res) {
     var param = req.body;
-    // res.send(req.body);
     res.send(param);
-    //simple query
-    // pool.request().query("exec GetSurat "+query, (err, result) => {
-    //     console.dir(result)
-    //     res.send(result);
-    // })
 });
 
 
-app.get('/tim', function (req, res) {
-    // var param = req.body;
-    // res.send(req.body);
-    // res.send(param);
-
+exp.get('/tim', function (req, res) {
     var config = {
         user: req.session.user,
         password: req.session.password,
@@ -251,17 +127,8 @@ app.get('/tim', function (req, res) {
             res.send(result);
         })
     });
-    //simple query
-    // pool.request().query("select * from tim", (err, result) => {
-    //     console.dir(result)
-    //     res.send(result);
-    // })
 });
-app.get('/klasifikasi', function (req, res) {
-    // var param = req.body;
-    // res.send(req.body);
-    // res.send(param);
-
+exp.get('/klasifikasi', function (req, res) {
     var config = {
         user: req.session.user,
         password: req.session.password,
@@ -277,18 +144,10 @@ app.get('/klasifikasi', function (req, res) {
             res.send(result);
         })
     });
-    //simple query
-    // pool.request().query("select * from klasifikasi", (err, result) => {
-    //     console.dir(result)
-    //     res.send(result);
-    // })
 });
 
-app.post('/generate-surat', function (req, res) {
+exp.post('/generate-surat', function (req, res) {
     var param = req.body;
-    // res.send(req.body);
-    // res.send(param);
-
     var config = {
         user: req.session.user,
         password: req.session.password,
@@ -304,14 +163,8 @@ app.post('/generate-surat', function (req, res) {
             res.send(result);
         });
     });
-
-    //simple query
-    // pool.request().query(`exec AddSurat @nomor_surat = '${param.nomor_surat}', @tujuan='${param.tujuan}', @perihal='${param.perihal}', @jumlah=${param.jumlah}`, (err, result) => {
-    //     console.dir(result)
-    //     res.send(result);
-    // });
 });
 
-var server = app.listen(5000, function () {
+var server = exp.listen(5000, function () {
     console.log('Server is running..');
 });
