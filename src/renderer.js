a47dbe851a3876486d949d2ce926373dc7743b43
mziyadam <mziyadam@users.noreply.github.com>
2023-10-03 20:58:38 +0700

    udh jd

--- src/renderer.js
+++ src/renderer.js
@@ -1,10 +1,3 @@
-// const setButton = document.getElementById('btn')
-// const titleInput = document.getElementById('title')
-// setButton.addEventListener('click', () => {
-//     const title = titleInput.value
-//     window.electronAPI.setTitle(title)
-// })
-
 const daftarSuratBtn = document.getElementById('daftar-surat')
 daftarSuratBtn.addEventListener('click', () => {
     const childWindow = window.open('daftar_surat/daftar-surat.html', 'modal')
